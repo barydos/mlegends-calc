73cf124dda56072bc0c241e3f8d20f4fcf46418a
Barry Wu <barryxwu@gmail.com>
2022-01-16 21:09:06 +1100

    feat: add remaining ele weapons and fix Types

--- components/Weapon.tsx
+++ components/Weapon.tsx
@@ -6,17 +6,58 @@ import { Jobs } from './Character';
 
 interface WeaponOption {
     value: number,
-    label: string | JSX.Element,
-    element: Element
+    label: string | JSX.Element
 }
 const weapons = [
     { value: 0, label: 'None', element: Element.NEUTRAL },
     { value: 1, label: (
         <>
-            <span id='element'>Elemental Staff/Wand 1</span>
-            <UncontrolledTooltip target='element' autohide={false} placement='bottom'>Neutral</UncontrolledTooltip>
+            <span id='ele-weapon-1'>Elemental Staff/Wand 1</span>
+            <UncontrolledTooltip target='ele-weapon-1' autohide={false} placement='bottom'>+25% Fire +10% Poison</UncontrolledTooltip>
         </>
-    ), element: Element.ICE }
+    )},
+    { value: 2, label: (
+        <>
+            <span id='ele-weapon-2'>Elemental Staff/Wand 2</span>
+            <UncontrolledTooltip target='ele-weapon-2' autohide={false} placement='bottom'>+25% Poison +10% Fire</UncontrolledTooltip>
+        </>
+    )},
+    { value: 3, label: (
+        <>
+            <span id='ele-weapon-3'>Elemental Staff/Wand 3</span>
+            <UncontrolledTooltip target='ele-weapon-3' autohide={false} placement='bottom'>+25% Ice +10% Lightning</UncontrolledTooltip>
+        </>
+    )},
+    { value: 4, label: (
+        <>
+            <span id='ele-weapon-4'>Elemental Staff/Wand 4</span>
+            <UncontrolledTooltip target='ele-weapon-4' autohide={false} placement='bottom'>+25% Lightning +10% Ice</UncontrolledTooltip>
+        </>
+    )},
+    { value: 5, label: (
+        <>
+            <span id='ele-weapon-5'>Elemental Staff/Wand 5</span>
+            <UncontrolledTooltip target='ele-weapon-5' autohide={false} placement='bottom'>+25% Fire +10% Poison</UncontrolledTooltip>
+        </>
+    )},
+    { value: 6, label: (
+        <>
+            <span id='ele-weapon-6'>Elemental Staff/Wand 6</span>
+            <UncontrolledTooltip target='ele-weapon-6' autohide={false} placement='bottom'>+25% Poison +10% Fire</UncontrolledTooltip>
+        </>
+    )},
+    { value: 7, label: (
+        <>
+            <span id='ele-weapon-7'>Elemental Staff/Wand 7</span>
+            <UncontrolledTooltip target='ele-weapon-7' autohide={false} placement='bottom'>+25% Ice +10% Lightning</UncontrolledTooltip>
+        </>
+    )},
+    { value: 8, label: (
+        <>
+            <span id='ele-weapon-8'>Elemental Staff/Wand 8</span>
+            <UncontrolledTooltip target='ele-weapon-8' autohide={false} placement='bottom'>+25% Lightning +10% Ice</UncontrolledTooltip>
+        </>
+    )}
 ]
 
 const defaultMultiplier = {
@@ -49,25 +90,25 @@ const Weapon = ({ weapon: weaponProp, loaded }: { weapon: Weapon | null, loaded:
         setWeapon(e.value);
         if (e.value === 1 || e.value === 5) {
             // +25% fire, +10% poison
-            setMultiplier({...defaultMultiplier, [Element.FIRE]: 1.25, [Element.POISON]: 1 })
+            setMultiplier({...defaultMultiplier, [Element.FIRE]: 1.25, [Element.POISON]: 1.1 })
         }
         if (e.value === 2 || e.value === 6) {
             // +25% poison, +10% fire
-            setMultiplier({...defaultMultiplier, [Element.POISON]: 1.25, [Element.FIRE]: 1 })
+            setMultiplier({...defaultMultiplier, [Element.POISON]: 1.25, [Element.FIRE]: 1.1 })
         }
         if (e.value === 3 || e.value === 7) {
             // +25% ice, + 10% lightning
-            setMultiplier({...defaultMultiplier, [Element.ICE]: 1.25, [Element.LIGHTNING]: 1 })
+            setMultiplier({...defaultMultiplier, [Element.ICE]: 1.25, [Element.LIGHTNING]: 1.1 })
         }
         if (e.value === 4 || e.value === 8) {
             // +25% lightning, +10% ice
-            setMultiplier({...defaultMultiplier, [Element.LIGHTNING]: 1.25, [Element.ICE]: 1 })
+            setMultiplier({...defaultMultiplier, [Element.LIGHTNING]: 1.25, [Element.ICE]: 1.1 })
         }
     }
     return (
         <div className='card'>
             <p>Elemental weapons</p>
-            <Select options={weapons} value={weapons.find(item => item.value === weapon)} placeholder='For magicians only' 
+            <Select instanceId='elem-weapon-dropdown' options={weapons} value={weapons.find(item => item.value === weapon)} placeholder='For magicians only' 
                 onChange={handleWeapon}
                 isDisabled={info.character && info.character.job !== Jobs.MAGICIAN}/>
         </div>
